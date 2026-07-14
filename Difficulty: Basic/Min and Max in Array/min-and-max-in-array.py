f83c336a1b06d4a55a12d6cbfa3bb96d8cd178d8
Abhinav Gupta <justavi.3289@gmail.com>
2026-07-14 09:59:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Min and Max in Array/min-and-max-in-array.py
+++ Difficulty: Basic/Min and Max in Array/min-and-max-in-array.py
@@ -1,19 +1,6 @@
 class Solution:
     def getMinMax(self, arr):
-
-        return [min(arr), max(arr)]
+        # code here
+        return [min(arr),max(arr)]
         
-# class Solution:
-#     def getMinMax(self, arr):
-
-#         mini = arr[0]
-#         maxi = arr[0]
-
-#         for i in range(len(arr)):
-#             if arr[i] < mini:
-#                 mini = arr[i]
-
-#             if arr[i] > maxi:
-#                 maxi = arr[i]
-
-#         return [mini, maxi]
+        
