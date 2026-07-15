0b5a659c8f100a24ebd856eb4bce265e8fe3b008
Abhinav Gupta <justavi.3289@gmail.com>
2026-07-15 22:19:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates from an array/remove-duplicates-from-an-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def remDuplicate(self, arr):
+        # code here 
+        seen = set()
+        abc=[]
+        for num in arr:
+            if num not in seen:
+                seen.add(num)
+                abc.append(num)
+        return abc
