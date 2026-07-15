c256a22d2d675099ec892afb50ef44f77b30bc6a
Abhinav Gupta <justavi.3289@gmail.com>
2026-07-15 22:21:57 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.py
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.py
@@ -1,14 +1,11 @@
 class Solution:
     def removeDuplicates(self, arr):
         # code here 
-        if len(arr)==0:
-            return []
-            
-        j=0
+        seen=set()
+        abc=[]
+        for num in arr:
+            if num not in seen:
+                seen.add(num)
+                abc.append(num)
+        return abc
         
-        for i in range(1,len(arr)):
-            if arr[i]!=arr[j]:
-                j+=1
-                arr[j]=arr[i]
-            
-        return arr[:j+1]
